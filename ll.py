62c91818669af51edacceb3e52c344fb04c09c40
Girish Raghav <girish.raghav2004@gmail.com>
2025-04-19 09:32:22 +0530

    sfdgj

---
+++ ll.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+    def insert(self,data):
+        newNode = Node(data);
+        if self.head is None:
+            self.head=newNode
+            self.tail=newNode
+        else:
+            self.tail.next=newNode
+            self.tail=newNode
+    def insertatbeg(self,data):
+        newNode = Node(data);
+        if self.head is None:
+            self.head=newNode
+            self.tail=newNode
+        else:
+            newNode.next=self.head
+            self.head=newNode
+    def insertatmid(self,pos,data):
+        newNode = Node(data);
+        if pos==0:
+            self.insertatbeg(data)
+        else:
+            temp=self.head
+            for i in range(pos-1):
+                temp=temp.next
+            newNode.next=temp.next
+            temp.next=newNode
+            
+    def print(self):
+        temp=self.head
+        while temp is not None:
+            print(temp.data,end=" ")
+            temp=temp.next
+
+if __name__=="__main__":
+    ll=LinkedList()
+    ele = list(map(int,input().split()))
+    for e in ele:
+        if e==-1:
+            break
+        ll.insertatbeg(e)
+    ll.print()
+    print("\n\n")
+    ll.insertatmid(3,10)
+    ll.print()
